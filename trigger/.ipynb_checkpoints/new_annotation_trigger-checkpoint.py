55f8681b8272ba3555abc27a4413d366c08208e2
Gcloud Long Test <gcloudtestlong@gmail.com>
2022-02-14 06:56:28 +0000

    main/ cat_classification: pipeline working

---
+++ trigger/.ipynb_checkpoints/new_annotation_trigger-checkpoint.py
@@ -0,0 +1,30 @@
+from kfp.v2.google.client import AIPlatformClient
+import os
+
+PROJECT_ID = "famous-momentum-338708"
+REGION = 'us-central1'
+GCBUCKET = "gs://long_cat_classification"
+PIPELINE_ROOT = os.path.join(GCBUCKET, "pipeline")
+PIPELINE_SPEC_PATH = os.path.join("./", "pipeline", "spec", "pipeline.json")
+
+def vertex_ai_pipeline_trigger(event, context):
+    file_name = event.name["name"]
+    print("File: {}".format(file_name))
+    print("Extension: ()".format(file_name.split(".")[-1]))
+    
+    if file_name.split(".")[-1] == "jsonl":
+        print("Target file extension changed")
+        
+        api_client = AIPlatformClient(
+            project_id=PROJECT_ID,
+            region=REGION
+        )
+        
+        print("api_client is successfully instantiated")
+        # Retrain model (use same pipeline spec)
+        response = api_client.create_run_from_job_spec(
+            f"{PIPELINE_SPEC_PATH}",
+            pipeline_root=f"{PIPELINE_ROOT}",
+            parameter_values={"project": PROJECT_ID}
+        )        
+        print("Response: {}".format(response))
